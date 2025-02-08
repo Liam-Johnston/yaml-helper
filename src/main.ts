300846a301531153e14cd9e708fbc25d6300a33b
Liam Johnston <liam.johnston@v2.digital>
2025-02-08 12:55:57 +1100

    test

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,6 @@ import * as core from '@actions/core'
 
 export const run = async () => {
   const ms: string = core.getInput('milliseconds')
-  console.log(ms)
+
+  core.info(`milliseconds input is: ${ms}`)
 }
